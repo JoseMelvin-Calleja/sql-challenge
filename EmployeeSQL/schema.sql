6dd561d81510c1fa38d1669f54a55dc3e1650af3
JoseMelvin-Calleja <jcalleja@bates.edu>
2023-02-28 20:41:45 -0500

    add not null and varchar(255)

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,39 +1,39 @@
 -- create table of departments 
 create table departments (
-	dept_no varchar primary key,
-	dept_name varchar
+	dept_no varchar(255) primary key,
+	dept_name varchar(255) not null
 );
 
 -- create table of titles
 create table titles (
-	title_id varchar primary key,
-	title varchar
+	title_id varchar(255) primary key,
+	title varchar(255) not null
 );
 
 -- create table of employees
 create table employees (
 	emp_no int primary key,
-	emp_title_id varchar not null,
+	emp_title_id varchar(255) not null,
 	foreign key (emp_title_id) references titles(title_id),
-	birth_date varchar,
-	first_name varchar,
-	last_name varchar,
-	sex varchar,
-	hire_date varchar
+	birth_date varchar(255),
+	first_name varchar(255) not null,
+	last_name varchar(255) not null,
+	sex varchar(255),
+	hire_date varchar(255) not null
 );
 
 -- create table of dept_emp
 create table dept_emp (
 	emp_no int not null,
 	foreign key (emp_no) references employees(emp_no),
-	dept_no varchar not null,
+	dept_no varchar(255) not null,
 	foreign key (dept_no) references departments(dept_no),
 	primary key(emp_no, dept_no)
 );
 
 -- creat table of dept_manager
 create table dept_manager (
-	dept_no varchar not null,
+	dept_no varchar(255) not null,
 	foreign key (dept_no) references departments(dept_no),
 	emp_no int not null,
 	foreign key (emp_no) references employees(emp_no),
